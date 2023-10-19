8808466273adfb139b26dbf146682ec692ed817b
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2023-10-19 15:58:28 +0300

    new

--- task-1.js
+++ task-1.js
@@ -1,3 +1,9 @@
 'use strict';
-console.log("JavaScript is awesome!");
-console.log(10);
+let quantity = 50;  //кількість замовлених дроїдів
+let pricePerDroid = 25000; //ціна одного дроїда
+
+"You ordered <quantity> droids worth <totalPrice> credits!"
+makeTransaction(quantity, totalPrice);
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
