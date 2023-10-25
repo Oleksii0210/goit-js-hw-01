2046627f31e6d10cdd7227630389342b1a2b85e5
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2023-10-25 23:37:26 +0300

    fix

--- task-1.js
+++ task-1.js
@@ -1,9 +1,8 @@
 'use strict';
-let quantity = 50;  //кількість замовлених дроїдів
-let pricePerDroid = 25000; //ціна одного дроїда
-
-"You ordered <quantity> droids worth <totalPrice> credits!"
-makeTransaction(quantity, totalPrice);
+function makeTransaction (quantity, pricePerDroid) {
+    const product = quantity * pricePerDroid;
+}
+return probuct;
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
--- task-3.js
+++ task-3.js
@@ -1,5 +1,11 @@
 'use strict';
-getElementWidth(content, padding, border);
+function getElementWidth(content, padding, border) {
+    let contentNumber = Number.parseFloat(content);
+    let paddingNumber = Number.parseFloat(padding);
+    let borderNumber = Number.parseFloat(border);
+    const getElementWidth = contentNumber + paddingNumber * 2 + borderNumber * 2;
+    return getElementWidth; 
+}
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
